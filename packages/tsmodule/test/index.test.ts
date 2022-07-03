48156cc1aa7a67e7735e35c696e5bbcd069df6cb
bent10 <bent10@stilearning.com>
2022-07-03 14:00:34 +0700

    test: replace ava with jest

--- packages/tsmodule/test/index.test.ts
+++ packages/tsmodule/test/index.test.ts
@@ -1,6 +1,5 @@
-import test from 'ava'
 import { sum } from '../dist/index.js'
 
-test('adds 1 + 2 to equal 3', t => {
-  t.is(sum(1, 2), 3)
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3)
 })
